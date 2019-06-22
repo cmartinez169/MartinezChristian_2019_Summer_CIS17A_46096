78f6c5c50197857f097df3ba5769a76c58191573
cmartinez169 <cmartinez169@student.rccd.edu>
2019-06-22 02:00:04 -0700

    Signed-off-by: cmartinez169 <cmartinez169@student.rccd.edu>

---
+++ Hmwk/CODE-E/Code-E_HelloWorld/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * Author: Christian Martinez 
+ * Date:6/22/2019
+ * Purpose:Create a Hello World Program
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Constants like PI, e, Gravity, Conversions, 2D array size only!
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) 
+    {
+        cout << "\n";
+        cout << "   Hello World!  \n" << endl;
+        
+        
+        return 0;
+    }
