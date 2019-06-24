99d2a490a0f7538a5c59c3419ee8eea5cb5de882
cmartinez169 <cmartinez169@student.rccd.edu>
2019-06-23 20:01:39 -0700

    Signed-off-by: cmartinez169 <cmartinez169@student.rccd.edu>

---
+++ Lab/IntroLab/Hello World/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Christian Martinez
+ * CIS 17A 46096
+ * * Created on June 19, 2019, 11:52 PM
+ */
+
+#include<iostream> //declaratio on input output function libraries 
+  
+using namespace std; 
+  
+ 
+int main() 
+{ 
+    
+    cout<<"Hello World"; //writes out hello world
+      
+    return 0; 
+} 
